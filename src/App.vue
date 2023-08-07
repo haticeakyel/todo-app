81208d5d81ae9f58efe37fed7ceb84bdd2cea633
Hatice Akyel <haticeeakyel@gmail.com>
2023-08-07 16:52:03 +0300

    list and delete tasks

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,90 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best Todo Ever!
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="background.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
       <v-toolbar-title>Vuetify Todo</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
     </v-main>
-
-    <v-navigation-drawer v-model="drawer">
-      <v-list dense nav class="menu-list" :class="{ 'visible': rail }">
-        <v-list-item-group>
-          <v-list-item v-for="item in menuItems" :key="item.value">
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
-<style>
-.menu-list {
-  display: none; /* Hide the menu list by default */
-}
-
-.visible {
-  display: block; /* Show the menu list when the 'visible' class is present */
-}
-</style>
-
-<script setup>
-import { ref } from 'vue'
-import router from './router'
-
-const drawer = ref(false)
-const rail = ref(false)
-
-const menuItems = [
-  { icon: 'mdi-home-city', title: 'Todo', value: 'home' },
-  { icon: 'mdi-account', title: 'About', value: 'account' },
-]
-
-const toggleDrawer = () => {
-  drawer.value = !drawer.value
-  rail.value = drawer.value
-}
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+        { title: 'About', icon: 'mdi-help-box', to: '/about' },
+      ],
+    }),
+  }
 </script>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="home">
+
+    <v-list 
+    flat
+    class="pt-0" 
+    > 
+    <div
+    v-for="task in tasks"
+      :key="task.id">
+      <v-list-item
+      @click="doneTask(task.id)"
+      :class="{'blue lighten-5': task.done}"
+      >
+        <template>
+          <v-list-item-action>
+            <v-checkbox 
+            :input-value= "task.done"
+            ></v-checkbox>
+          </v-list-item-action>
+       
+        <v-list-item-content>
+          <v-list-item-title
+          :class="{'text-decoration-line-through':task.done}"
+          >{{ task.title }}</v-list-item-title>
+        </v-list-item-content>
+
+        
+      <v-list-item-action>
+          <v-btn
+          @click="deleteTask(task.id)"
+          icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+        </v-btn>
+        </v-list-item-action>
+      </template>
+
+
+      </v-list-item>
+    <v-divider></v-divider>
+    </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+    data() {
+      return{
+        tasks: [
+          {
+            id:1, 
+            title: 'Wake Up',
+            done:false
+          },
+          {
+            id:2, 
+            title: 'Wash Your Face',
+            done:true
+          },
+          {
+            id:3, 
+            title: 'Brush Your Teeth',
+            done:false
+          },
+          {
+            id:4, 
+            title: 'Have breakfast',
+            done:false
+          },
+        ]
+      }
+    },
+    methods:{
+      doneTask(id){
+        let task = this.tasks.find(task => task.id === id)
+        if (task) {
+        task.done = !task.done;
+        }
+      },
+      deleteTask(id){
+        this.tasks = this.tasks.filter(task=> task.id !== id)
+      },
+    }
+  
+  }
+</script>
